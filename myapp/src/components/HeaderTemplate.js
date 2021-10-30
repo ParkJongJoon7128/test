91f8456fb89a20ae63a1f0fb213e32d3e4af646d
박 종준 <whdwnsdk8256@naver.com>
2021-10-30 14:54:15 +0000

    making login and sign-up system

--- myapp/src/components/HeaderTemplate.js
+++ myapp/src/components/HeaderTemplate.js
@@ -1,39 +1,36 @@
 import React from 'react';
 
 const HeaderTemplate = () => {
-	
 	const Header = {
 		backgroundColor: '#0bb286',
-        color: '#fff',
-        fontSize: '32px',
-        padding: '20px',
-        width: '100%',
-		height: '5vh'
-	}
-	
+		color: '#fff',
+		fontSize: '32px',
+		padding: '20px',
+		width: '100%',
+		height: '5vh',
+	};
+
 	const Signin = {
-		backgroundColor: '#00bcd4',
-		color: "#fff",
+		backgroundColor: '#0bb286',
+		color: '#fff',
 		fontSize: '20px',
-		width: '150px',
+		width: '100px',
 		height: '5vh',
 		float: 'right',
 		border: 'none',
 		borderRadius: '10px',
 		outline: 'none',
-		cursor: 'pointer'
-	}
-	
-    return (
-       <div className = "Header" style={Header}>
+		cursor: 'pointer',
+	};
+
+	return (
+		<div className="Header" style={Header}>
 			인턴공고 및 학교공지 플랫폼 사이트
-			<button className = "Signin" style = {Signin} Link to = "Signin">Sign-in</button>
+			<button className="Signin" style={Signin} onClick="">
+				Sign-in
+			</button>
 		</div>
-    );
-}
+	);
+};
 
 export default HeaderTemplate;
-
-
-
-
