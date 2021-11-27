42aae5b3d866e87dd3afefdfd4af954c7e250a6b
박 종준 <whdwnsdk8256@naver.com>
2021-11-27 02:01:55 +0000

    make sign in Redirect

--- myapp/src/components/LoginForm.js
+++ myapp/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, Route, Switch, BrowserRouter, Redirect as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 const axios = require('axios');
 //import '/workspace/test/myapp/src/styles/LoginForm.css';
 
@@ -8,17 +8,21 @@ const LoginForm = () => {
 	const [password, setPassword] = useState('');
 	const [userList, setuserList] = useState([]);
 
+	const [isLoggedin, setisLoggedin] = useState(false);
+
 	const getusers = () => {
 		axios.get('https://kangnam-site.herokuapp.com/seeusers').then((response) => {
 			response.data.filter((users) => {
 				if (users.email.includes(email)) {
 					if (users.pass === password) {
+						console.log('Logged in');
 						let myObj = {
 							firstname: users.firstname,
 							lastname: users.lastname,
 							email: users.email,
 						};
 						localStorage.setItem('savesesion', JSON.stringify(myObj));
+						setisLoggedin(true);
 					}
 				}
 			});
@@ -66,14 +70,13 @@ const LoginForm = () => {
 			<p className="forgot-password text-right">
 				Forgot <a href="#">password?</a>
 			</p>
-			<Link to="/">
-				<button onClick={getusers} className="btn btn-primary btn-block">
-					Sign In
-				</button>
-			</Link>
+			<button onClick={getusers} className="btn btn-primary btn-block">
+				Sign In
+			</button>
 			<div>
 				You dont have a account? Click the <Link to="/Signup">sign up</Link>
 			</div>
+			{isLoggedin ? <Redirect to="/" /> : ''}
 		</div>
 	);
 };
