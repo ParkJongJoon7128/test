0f5a40a74d93139ddb1b056922cdabffcc9735f4
박 종준 <whdwnsdk8256@naver.com>
2021-11-27 14:23:16 +0000

    make logout

--- myapp/src/components/HeaderTemplate.js
+++ myapp/src/components/HeaderTemplate.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
-const HeaderTemplate = () => {
+const HeaderTemplate = (props) => {
 	const Header = {
 		backgroundColor: '#0bb286',
 		color: '#fff',
 		fontSize: '32px',
 		padding: '20px',
 		width: '100%',
-		height: '10vh'
+		height: '10vh',
 	};
 
 	const Login = {
@@ -24,14 +25,44 @@ const HeaderTemplate = () => {
 		cursor: 'pointer',
 	};
 
+	const logged_in = {
+		color: '#fff',
+		fontSize: '20px',
+		width: '100px',
+		height: '10vh',
+		float: 'right',
+		outline: 'none',
+		cursor: 'pointer',
+	};
+
+	const Hidden = {
+		display: 'none',
+	};
+
+	const logout = () => {
+		let myObj = {
+			firstname: '',
+			lastname: '',
+			email: '',
+		};
+		localStorage.setItem('savesesion', JSON.stringify(myObj));
+		window.location.reload();
+	};
+
 	return (
 		<div className="Header" style={Header}>
 			인턴공고 및 학교공지 플랫폼 사이트
 			<Link to="/Login">
-				<button className="Login" style={Login}>
+				<button className="Login" style={props.myuser ? Hidden : Login}>
 					Sign-in
 				</button>
 			</Link>
+			<div className="logged_in" style={props.myuser ? logged_in : Hidden}>
+				<DropdownButton id="dropdown-basic-button" title={props.myuser}>
+					<Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
+					<Dropdown.Item href="#/action-2">My Page</Dropdown.Item>
+				</DropdownButton>
+			</div>
 		</div>
 	);
 };
