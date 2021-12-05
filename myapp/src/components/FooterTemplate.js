bbc08ad2b9ad0336030b6703e3d0ccfc5611bc17
박 종준 <whdwnsdk8256@naver.com>
2021-12-05 10:51:16 +0000

    Design all site (ui/ux)

--- myapp/src/components/FooterTemplate.js
+++ myapp/src/components/FooterTemplate.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import styles from '../styles/FooterTemplate.css';
-import insta from '../images/instagram.png';
+import { FiInstagram } from 'react-icons/fi';
+import { BsFacebook } from 'react-icons/bs';
+import { SiNotion } from 'react-icons/si';
 
 const FooterTemplate = () => {
 	return (
 		<div className="Footer" style={styles.Footer}>
 			2021 © Park Jong Joon.
-			<div>
-				<img src={insta} className="instagram" alt="instagram" style={styles.instagram} />
+			<div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+				<div
+					style={{
+						backgroundColor: '#fff',
+						width: '45px',
+						height: '45px',
+						borderRadius: '50%',
+						marginRight: '10px'
+					}}
+				>
+					<FiInstagram color={'#320bb2'} size={30} style= {{marginTop: 6}} />
+				</div>
+				<div
+					style={{
+						backgroundColor: '#fff',
+						width: '45px',
+						height: '45px',
+						borderRadius: '50%',
+						marginRight: '10px'
+					}}
+				>
+					<BsFacebook color={'#320bb2'} size={30} style= {{marginTop: 6}}  />
+				</div>
+				<div
+					style={{
+						backgroundColor: '#fff',
+						width: '45px',
+						height: '45px',
+						borderRadius: '50%',
+						marginRight: '10px'
+					}}
+				>
+					<SiNotion color={'#320bb2'} size={30} style= {{marginTop: 6}}  />
+				</div>
 			</div>
 		</div>
 	);
