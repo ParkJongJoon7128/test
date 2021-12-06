6112ee364d808583b88c9cddadb21b8352028ce3
박 종준 <whdwnsdk8256@naver.com>
2021-12-06 14:14:03 +0000

    Design all site (ui/ux)

--- myapp/src/components/FooterTemplate.js
+++ myapp/src/components/FooterTemplate.js
@@ -7,18 +7,24 @@ import { SiNotion } from 'react-icons/si';
 const FooterTemplate = () => {
 	return (
 		<div className="Footer" style={styles.Footer}>
-			2021 © Park Jong Joon.
-			<div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+			Copyright Ⓒ 2021. Park Jong Joon. All right reserved.
+			<div style={{ display: 'flex', justifyContent: 'center', marginTop: '22px' }}>
 				<div
 					style={{
 						backgroundColor: '#fff',
 						width: '45px',
 						height: '45px',
 						borderRadius: '50%',
-						marginRight: '10px'
+						marginRight: '10px',
 					}}
+					className="FiInstagram"
+					onClick={ (e) =>  window.location.href='https://www.instagram.com/jong._.joon/?hl=ko'}
 				>
-					<FiInstagram color={'#320bb2'} size={30} style= {{marginTop: 6}} />
+					<FiInstagram
+						color={'#320bb2'}
+						size={30}
+						style={{ marginTop: 7, marginLeft: 1 }}
+					/>
 				</div>
 				<div
 					style={{
@@ -26,10 +32,16 @@ const FooterTemplate = () => {
 						width: '45px',
 						height: '45px',
 						borderRadius: '50%',
-						marginRight: '10px'
+						marginRight: '10px',
 					}}
+					className="BsFacebook"
+					onClick={ (e) =>  window.location.href='https://www.facebook.com/profile.php?id=100008341442712'}
 				>
-					<BsFacebook color={'#320bb2'} size={30} style= {{marginTop: 6}}  />
+					<BsFacebook
+						color={'#320bb2'}
+						size={30}
+						style={{ marginTop: 7, marginLeft: 1 }}
+					/>
 				</div>
 				<div
 					style={{
@@ -37,10 +49,12 @@ const FooterTemplate = () => {
 						width: '45px',
 						height: '45px',
 						borderRadius: '50%',
-						marginRight: '10px'
+						marginRight: '10px',
 					}}
+					className="SiNotion"
+					onClick={ (e) =>  window.location.href='https://www.notion.so/ee2d05f506224bce859117314530b00a'}
 				>
-					<SiNotion color={'#320bb2'} size={30} style= {{marginTop: 6}}  />
+					<SiNotion color={'#320bb2'} size={30} style={{ marginTop: 7, marginRight: 1 }} />
 				</div>
 			</div>
 		</div>
